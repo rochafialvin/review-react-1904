25b04ebcf6cde9763ad7b3c45ffe63240302c14e
rochafialvin <rochafi.dev@gmail.com>
2022-01-11 15:09:29 +0700

    Send Action

--- src/components/Sender.js
+++ src/components/Sender.js
@@ -1,4 +1,21 @@
+import { useDispatch } from "react-redux";
+
 export default function Sender() {
+  const dispatch = useDispatch();
+
+  const onBtnSavingClick = () => {
+    const action = { type: "saving", payload: { cash: 100 } };
+    dispatch(action);
+  };
+  const onBtnWithdrawClick = () => {
+    const action = { type: "withdraw", payload: { amount: 50 } };
+    dispatch(action);
+  };
+  const onBtnRegisterClick = () => {
+    const action = { type: "register", payload: { name: "Uzui Tengen" } };
+    dispatch(action);
+  };
+
   return (
     <div
       style={{
@@ -7,9 +24,14 @@ export default function Sender() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
+        flexDirection: "column",
+        padding: "10px",
       }}
     >
       <h1>Sender One</h1>
+      <button onClick={onBtnSavingClick}>Saving : 100</button>
+      <button onClick={onBtnWithdrawClick}>Widthdraw : 50</button>
+      <button onClick={onBtnRegisterClick}>Register : Uzui Tengen</button>
     </div>
   );
 }
